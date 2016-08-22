37ec02ddb4713a22501d0815aac2bceab6e4380c
10917 <yongjia.liu01@hand-china.com>
2016-08-23 00:21:25 +0800

    add a stupid tableview to show wrong data

--- exHenTai/MainViewController.swift
+++ exHenTai/MainViewController.swift
@@ -9,28 +9,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    var tableView: UITableView!
+    var nodes = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = NSURL(string: innerSpace)
-        let data = NSData(contentsOfURL: url!)
         
-        let parser = TFHpple(HTMLData: data!)
-        let queryString = "//div[@class='ig']/table/tr/td//table[@class='it']/tr/td/a"
-        let nodes = parser.searchWithXPathQuery(queryString)
+        self.loadData()
+        let tableView = UITableView(frame: self.view.frame)
+        self.view.addSubview(tableView);
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "HTCell")
         
-        //var newItems = NSMutableArray(capacity: 0);
+        self.tableView = tableView
         
-        for node in nodes {
-            if let element = node.firstChild as TFHppleElement! {
-                print(element.content)
-            }
-            //print(node)
-        }
-        
-        print(nodes.count)
-        //print(nodes)
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,5 +32,48 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loadData() {
+        
+        let url = NSURL(string: innerSpace)
+        let data = NSData(contentsOfURL: url!)
+        
+        let parser = TFHpple(HTMLData: data!)
+        let queryString = "//div[@class='ig']/table/tr/td//table[@class='it']/tr/td/a"
+        nodes = parser.searchWithXPathQuery(queryString)
+        
+        for node in nodes {
+            if let element = node.firstChild as TFHppleElement! {
+                print(element.content)
+            }
+        }
+    }
+    
 
+}
+
+
+extension MainViewController: UITableViewDelegate {
+    
+}
+
+extension MainViewController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nodes.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("HTCell")
+        let node = nodes[indexPath.row]
+        if let element = node.firstChild as TFHppleElement! {
+            if element.content != "Go To First Page" {
+                cell?.textLabel?.text = element.content
+            }
+            
+        }
+        return cell!
+    }
 }
